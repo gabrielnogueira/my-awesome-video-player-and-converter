ef2a2b292b41ddfc9c5b3a49c123807bd8e1fe21
gabrielnogueira <infotisolutions@gmail.com>
2018-06-16 19:31:20 -0300

    Initial commit

---
+++ server/boot/root.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var path = require('path');
+
+module.exports = function (server) {
+  var router = server.loopback.Router();
+  // Install a `/status` route that returns server status
+  router.get('/status', server.loopback.status());
+
+  router.get('/', function (req, res) {
+    var indexFile = path.resolve(__dirname, '../..', server.get('indexFile'));
+    res.sendFile(indexFile);
+  });
+  server.use(router);
+};
