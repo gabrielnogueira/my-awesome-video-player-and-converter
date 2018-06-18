f810fd2aa0c1bfde115c2cac11fb969920054f3e
gabrielnogueira <infotisolutions@gmail.com>
2018-06-18 01:34:09 -0300

    Improving basic Rest Services

--- server/models/video.js
+++ server/models/video.js
@@ -1,18 +1,35 @@
 'use strict';
+const path = require('path');
+const fs = require('fs');
+const progress = require('progress-stream');
+const constants = require('../../common/constants')
 
-module.exports = function (Video) {
+module.exports = Video => {
 
-  Video.listAll = function (cb) {
-    cb(null, {
-      message: "List of all videos"
-    });
-  }
+  Video.listAll = callback => {
+    const {
+      PersistentVideo
+    } = Video.app.models;
 
+    PersistentVideo.find((err, videos) => {
+      callback(null, {
+        data: videos
+      })
+    })
+
+  };
   Video.remoteMethod('listAll', {
     description: "List all videos",
     returns: [{
       arg: 'result',
-      type: 'object'
+      type: 'object',
+      default: [{
+        name: 'Video 1',
+        status: 'FINALIZADO'
+      }, {
+        name: 'Video 2',
+        status: 'ENVIANDO'
+      }]
     }, ],
     http: {
       path: '/',
@@ -20,12 +37,56 @@ module.exports = function (Video) {
     },
   });
 
-  Video.listById = function (id, cb) {
-    cb(null, {
-      message: "A video id: " + id
-    });
-  }
+  Video.beforeRemote('listAllChangeStream', (ctx, unused, next) => {
+    const {
+      PersistentVideo
+    } = Video.app.models;
+    PersistentVideo.createChangeStream((err, changes) => {
+      changes.on('data', change => {
+        const {
+          socket
+        } = Video.app;
 
+        Video.listAll((err, result) => {
+          socket.emit(constants.VIDEO_LIST_CHANGED, result);
+        })
+      })
+    });
+    next();
+  });
+  Video.listAllChangeStream = (callback) => {
+    Video.listAll(callback);
+  };
+  Video.remoteMethod('listAllChangeStream', {
+    description: "List all videos with change stream",
+    returns: [{
+      arg: 'result',
+      type: 'object',
+      default: [{
+        name: 'Video 1',
+        status: 'FINALIZADO'
+      }, {
+        name: 'Video 2',
+        status: 'ENVIANDO'
+      }]
+    }, ],
+    http: {
+      path: '/realtime-change',
+      verb: 'get'
+    },
+  });
+
+  Video.listById = (id, callback) => {
+    const {
+      PersistentVideo
+    } = Video.app.models;
+
+    PersistentVideo.findById(id, (err, video) => {
+      callback(null, {
+        data: video
+      })
+    })
+  };
   Video.remoteMethod('listById', {
     description: "List a video by id",
     accepts: [{
@@ -43,20 +104,78 @@ module.exports = function (Video) {
     },
   });
 
-  Video.new = function (data, cb) {
-    cb(null, "New video upload");
-  }
+  Video.new = (req, res, callback) => {
+    const {
+      PersistentVideo,
+      VideoFile
+    } = Video.app.models;
+    const file = req.files.content || req.files.file
 
+    fs.stat(file.path, function (err, stats) {
+      PersistentVideo.create({
+          name: file.name,
+          status: constants.VIDEO_STATUS_ENVIANDO
+        })
+        .then(createVideo => {
+
+          const {
+            socket
+          } = Video.app;
+
+          var writer = VideoFile.uploadStream('originals', `id${createVideo.id}-${file.name}`);
+
+          var prog = progress({
+            length: stats.size,
+            time: 1000
+          });
+
+          prog.on('progress', function (progress) {
+            socket.emit(constants.VIDEO_UPLOAD_PROGRESS, {
+              id: createVideo.id,
+              progress: progress.percentage
+            });
+          });
+
+          writer.on('error', function (err) {
+            console.log(err);
+          })
+
+          writer.on('finish', function (result) {
+            createVideo.patchAttributes({
+                status: constants.VIDEO_STATUS_ENCODANDO
+              })
+              .then(result => {
+                //call encoding service
+              });
+
+          });
+
+          fs.createReadStream(file.path)
+            .pipe(prog)
+            .pipe(writer);
+
+        });
+    });
+
+    callback(null, "Iniciou o Upload.");
+
+  };
   Video.remoteMethod('new', {
     description: "Include new video",
     accepts: [{
-        arg: 'data',
+        arg: 'req',
         type: 'object',
-        required: true,
         http: {
-          source: 'body'
-        },
+          source: 'req'
+        }
       },
+      {
+        arg: 'res',
+        type: 'object',
+        http: {
+          source: 'res'
+        }
+      }
     ],
     returns: {
       arg: 'result',
@@ -66,64 +185,7 @@ module.exports = function (Video) {
       path: '/',
       verb: 'post'
     },
-    
+
   });
 
-  Video.update = function (id, data, cb) {
-    cb(null, "Updated video id: " + id);
-  }
-
-  Video.remoteMethod('update', {
-    description: "Update existing or create new video in id",
-    accepts: [{
-        arg: 'id',
-        type: 'number',
-        required: true
-      }, {
-        arg: 'data',
-        type: 'object',
-        required: true,
-        http: {
-          source: 'body'
-        },
-      }
-    ],
-    returns: {
-      arg: 'result',
-      type: 'object'
-    },
-    http: {
-      path: '/:id',
-      verb: 'put'
-    },
-  });
-
-  Video.updatePartial = function (id, data, cb) {
-    cb(null, "Updated partial video id:" + id);
-  }
-
-  Video.remoteMethod('updatePartial', {
-    description: "Update properties of existing video by id",
-    accepts: [{
-        arg: 'id',
-        type: 'number',
-        required: true
-      }, {
-        arg: 'data',
-        type: 'object',
-        required: true,
-        http: {
-          source: 'body'
-        },
-      }
-    ],
-    returns: {
-      arg: 'result',
-      type: 'object'
-    },
-    http: {
-      path: '/:id',
-      verb: 'patch'
-    },
-  });
 };
