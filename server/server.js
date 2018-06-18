8466aa8f71f7699d7f09f3818381f30e0c40e96f
gabrielnogueira <infotisolutions@gmail.com>
2018-06-18 01:36:02 -0300

    Including socket.io and formidable support

--- server/server.js
+++ server/server.js
@@ -2,17 +2,20 @@
 
 var loopback = require('loopback');
 var boot = require('loopback-boot');
+var formidable = require('express-formidable');
 var path = require('path');
+var socket = require('socket.io')
 var app = module.exports = loopback();
 
-app.start = function() {
+app.start = function () {
   var staticFolder = path.dirname(
     path.resolve(__dirname, '..', app.get('indexFile'))
   );
   app.use(loopback.static(staticFolder));
-  
+  app.use(formidable());
+
   // start the web server
-  return app.listen(function() {
+  return app.listen(function () {
     app.emit('started');
     var baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
@@ -25,10 +28,18 @@ app.start = function() {
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
+boot(app, __dirname, function (err) {
   if (err) throw err;
 
   // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
+  if (require.main === module) {
+
+    app.socket = socket(app.start());
+    app.socket.on('connection', function (socket) {
+      console.log('a user connected');
+      socket.on('disconnect', function () {
+        console.log('user disconnected');
+      });
+    });
+  }
 });
