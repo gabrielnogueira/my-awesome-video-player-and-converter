89df20dc7dd3fdd6faa169336c64fafc3ffff20b
gabrielnogueira <infotisolutions@gmail.com>
2018-06-22 01:43:03 -0300

    Including router

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route} from  'react-router-dom';
+
 import './index.css';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <div>
+            <Route exact path='/' component={App} />
+            <Route exact path='/videos' component={App} />
+            <Route path='/videos/:id' component={App} />
+        </div>
+    </BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
